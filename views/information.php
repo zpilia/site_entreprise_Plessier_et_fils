be2e735a5aef0f2741b61c39d820086a249260d7
Zoé Pilia <61697702+zpilia@users.noreply.github.com>
2022-06-15 15:33:35 +0200

    modif

--- views/information.php
+++ views/information.php
@@ -91,7 +91,7 @@
             </div>
             <div class="col">
                 <div class="card">
-                    <img src="https://mdbcdn.b-cdn.net/img/new/standard/city/041.webp" class="card-img-top" alt="Evolutrans"/>
+                    <img src="../public/img/evolutrans-hlogo-quadri-baseline.jpeg" class="card-img-top" alt="Evolutrans"/>
                     <div class="card-body">
                         <h5 class="card-title">Evolutrans</h5>
                         <p class="card-text">
