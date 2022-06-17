74d0e708abd79223d5a319af4a97d977c90a4e1f
Zoé Pilia <61697702+zpilia@users.noreply.github.com>
2022-06-17 09:52:24 +0200

    modif marge

--- views/header.php
+++ views/header.php
@@ -1,8 +1,8 @@
 <header>
 
     <div class="row">
-        <div class="col text-center align-self-center ">
-            <h1 class="title-header" >Transports Plessier & Fils</h1>
+        <div class="col text-center align-self-center">
+            <h1 class="title-header mt-4 mb-4">Transports Plessier & Fils</h1>
         </div>
     </div>
 
