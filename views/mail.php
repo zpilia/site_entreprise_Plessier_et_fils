671266d23b3164dd08a699497029db7c00119e39
zpilia <61697702+zpilia@users.noreply.github.com>
2022-06-18 18:27:26 +0200

    modif

--- views/mail.php
+++ views/mail.php
@@ -1,1 +1,18 @@
 <?php
+
+echo "<pre>";
+
+    print_r($_POST);
+
+    $name = $_POST['login'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $text = $_POST['content'];
+
+    $to = "";
+
+
+
+echo '<pre>';
+
+?>
