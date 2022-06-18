581e81cb97d7a1d7c6a7c247d100b52e54e13dda
zpilia <61697702+zpilia@users.noreply.github.com>
2022-06-18 23:36:57 +0200

    modif

--- views/recrutement.php
+++ views/recrutement.php
@@ -6,17 +6,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../public/css/style_bootstrap.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.css" rel="stylesheet"/>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.0.0/mdb.min.js"></script>
+    <script type="text/javascript" src=https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
     <title>Transports Plessier - Recrutement</title>
 </head>
 
 <?php
 
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 
 require 'header.php';
     require 'A:\wamp64\www\php-mysql\Projet_Perso\site_entreprise_Plessier_et_fils\PHPMailer\src\Exception.php';
@@ -27,34 +30,43 @@ require 'header.php';
 <?php
 
     $message_sent = false;
-    if (isset($_POST['send_mail'])) {
-        if (!empty($_POST['login'])) { // Nom
-            if (!empty($_POST['email'])) { // Email
-                if (!empty($_POST['subject'])) { // Objet
+    if (isset($_POST['send_mail_file'])) {
+        if (!empty($_POST['surname'])) { // Nom
+            if (!empty($_POST['name'])) { // Email
+                if (!empty($_POST['email'])) { // Objet
                     if (!empty($_POST['content'])) { //message
-                        $mail = new PHPMailer();
 
+                        $mail = new PHPMailer();
+                        echo "envoi mail";
                         try {
+
                             //Server settings
+                            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
                             $mail->isSMTP();                                            //Send using SMTP
                             $mail->Host       = 'smtp.mailtrap.io';                     //Set the SMTP server to send through
                             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                            $mail->Username   = 'dc678a16204ec3';                     //SMTP username
-                            $mail->Password   = 'e7e9824ff977e4';                               //SMTP password
+                            $mail->Username   = 'dc678a16204ec3';                       //SMTP username
+                            $mail->Password   = 'e7e9824ff977e4';                        //SMTP password
                             $mail->Port       = 2525;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
                             //Recipients
                             $mail->setFrom('zpilia2@gmail.com', 'Mailer');
-                            $mail->addAddress($_POST['email'], $_POST['login']);     //Add a recipient
-                            $mail->addReplyTo('zpilia2@gmail.com', 'Zoé');
+                            $mail->addAddress($_POST['email'], $_POST['name'] . " " . $_POST['surname']);     //Add a recipient
+                            $mail->addReplyTo('zpilia2@gmail.com', 'Zoe');
+
+                            $mail->addAttachment($_FILES['cv']['tmp_name'], $_FILES['cv']['name']);                         //Add attachments
+                            $mail->addAttachment($_FILES['motivationLetter']['tmp_name'], $_FILES['motivationLetter']['name']);          //Add attachments
 
                             //Content
                             $mail->isHTML(true);                                  //Set email format to HTML
-                            $mail->Subject = $_POST['subject'];
+                            $mail->Subject = "Nouvelle candidature !";
                             $mail->Body    = $_POST['content'];
                             $mail->AltBody = $_POST['content'];
 
                             $mail->send();
+                            ?>
+
+                            <?php
                             echo 'Message has been sent';
                         } catch (Exception $e) {
                             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
@@ -85,47 +97,50 @@ require 'header.php';
 
     <div class="row d-flex justify-content-center">
 
-        <div class="col-md-6">
+        <div class="col-md-10">
             <div class="card">
                 <div class="card-body">
-                    <form method="POST">
+                    <form action="" method="POST" enctype="multipart/form-data">
                         <div class="md-form">
                             <label for="form-surname">Nom</label>
-                            <input type="text" id="form-surname" class="form-control" name="surname">
+                            <input type="text" id="form-surname" class="form-control" name="surname" required>
                         </div>
                         <br>
+
                         <div class="md-form">
                             <label for="form-name">Prénom</label>
-                            <input type="text" id="form-name" class="form-control" name="name">
+                            <input type="text" id="form-name" class="form-control" name="name" required>
                         </div>
                         <br>
+
                         <div class="md-form">
                             <label for="form-email">Email</label>
-                            <input type="email" id="form-email" class="form-control" />
+                            <input type="email" id="form-email" class="form-control" name="email" required/>
                         </div>
                         <br>
+
                         <div class="md-form">
                             <label for="form-text">Message</label>
-                            <textarea id="form-text" class="form-control md-textarea" rows="3" name="content"></textarea>
+                            <textarea id="form-text" class="form-control md-textarea" rows="3" name="content" required></textarea>
                         </div>
                         <br>
-                        <div class="container">
-                            <div class="row justify-content-md-center">
-                                <div class="col-md-3">
-                                    <button type="button" class="btn btn-primary  btn-floating  btn-lg mb-2"><i class="fas fa-download"></i></button>
-                                    <p class="text-justify">CV</p>
-                                </div>
-                                <div class="col-md-3">
-                                    <button type="button" class="btn btn-primary  btn-floating  btn-lg mb-2"><i class="fas fa-download"></i></button>
-                                    <p class="text-justify">Lettre motivation</p>
-                                </div>
-                            </div>
+
+                        <div class="input-group mb-3">
+                            <label class="input-group-text" for="inputGroupFile01">CV</label>
+                            <input type="file" class="form-control" id="inputGroupFile01" name="cv" required>
                         </div>
                         <br>
+
+                        <div class="input-group mb-3">
+                            <label class="input-group-text" for="inputGroupFile01">Lettre de motivation</label>
+                            <input type="file" class="form-control" id="inputGroupFile01" name="motivationLetter" required>
+                        </div>
+                        <br>
+
                         <div class="container">
                             <div class="row justify-content-center">
                                 <div class="col-3">
-                                    <button type="submit" class="btn btn-primary">Envoyer</button>
+                                    <button name="send_mail_file" type="submit" class="btn btn-primary">Envoyer</button>
                                 </div>
                             </div>
                         </div>
